3c7e992cba5ce8ba1900551b3f2c94d01e1f26f0
zy <894298646@qq.com>
2018-10-23 11:24:48 +0800

    random value

--- src/main/java/com/atmoon/controller/HelloController.java
+++ src/main/java/com/atmoon/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String say(){
-        return girlProperties.getCity();
+        return girlProperties.getCity() + girlProperties.getAge();
     }
 
 }
